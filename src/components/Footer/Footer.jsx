7da0a4a81583d1b300f7eee7da895cb4725bf873
AbigailR.C <roabiguez@gmail.com>
2024-08-19 18:09:19 -0400

    Added API

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Developed by Abigail Rodriguez C.</p>
+      <p>2024</p>
+    </div>
+  );
+}
+
+export default Footer;
